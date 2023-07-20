82dc7408a14019ad0481c99448f11bee149ea4b7
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-07-20 10:14:51 +0530

    in and not in list functions

---
+++ in-&-not-in_list.py
@@ -0,0 +1,52 @@
+my_list = [0, 3, 12, 8, 2]
+
+print(5 in my_list)
+print(5 not in my_list)
+print(12 in my_list)
+
+#Let's do some complex for looping for the list
+
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = my_list[0]
+
+for i in range(1, len(my_list)):
+    if my_list[i] > largest:
+        largest = my_list[i]
+
+print(largest)
+
+#The code may be rewritten to make use of the newly introduced form of the for loop")
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = my_list[0]
+ 
+for i in my_list:
+    if i > largest:
+        largest = i
+ 
+print(largest)
+
+
+#If you need to save computer power, you can use a slice:
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = my_list[0]
+ 
+for i in my_list[1:]:
+    if i > largest:
+        largest = i
+ 
+print(largest)
+
+#Let's assume that you've chosen the following numbers in the lottery: 3, 7, 11, 42, 34, 49.
+#The numbers that have been drawn are: 5, 11, 9, 42, 3, 49.
+#The question is: how many numbers have you hit?
+
+drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
+
+for number in bets:
+    if number in drawn:
+        hits += 1
+
+print(hits)
+
