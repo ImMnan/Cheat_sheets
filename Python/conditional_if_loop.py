cee3e48d2e1f7f6c5fc58e977242f3e346b826ee
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-08-17 09:54:24 +0530

    conditional if loop in py

---
+++ Python/conditional_if_loop.py
@@ -0,0 +1,26 @@
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+ 
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+ 
+# Print the result
+print("The larger number is:", larger_number)
+
+
+# Read three numbers.
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+ 
+# Check which one of the numbers is the greatest
+# and pass it to the largest_number variable.
+ 
+largest_number = max(number1, number2, number3)
+ 
+# Print the result.
+print("The largest number is:", largest_number)
